b0afdf451d08aa3455d7f19ecf0c0c51dbc6a4b2
MarcoRipari <marco.ripari.falc@gmail.com>
2025-07-11 11:02:46 +0200

    Update generatore.py

--- generatore.py
+++ generatore.py
@@ -223,7 +223,7 @@ if uploaded:
 
     # Stimo il costo del token con RAG
     if st.button("💬 Mostra Prompt di Anteprima"):
-        test_row = 0
+        test_row = 1
         try:
             if sheet_id:
                 # Carica storico ed esegui FAISS
