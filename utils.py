51774f1d7330a125c7e20ba6828c9acac03dd34a
MarcoRipari <marco.ripari.falc@gmail.com>
2025-07-18 11:52:26 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+# utils.py
+
+import pandas as pd
+import chardet
+
+def read_csv_auto_encoding(uploaded_file):
+    raw_data = uploaded_file.read()
+    result = chardet.detect(raw_data)
+    encoding = result['encoding'] or 'utf-8'
+    uploaded_file.seek(0)
+    return pd.read_csv(uploaded_file, encoding=encoding)
+
+def not_in_array(array, required):
+    return not all(col in array for col in required)
