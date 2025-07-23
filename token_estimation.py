79c7fb15c45b3e25f5f2ad41cf7ce2a04ded1da7
MarcoRipari <marco.ripari.falc@gmail.com>
2025-07-23 11:29:07 +0200

    Delete token_estimation.py

--- token_estimation.py
+++
@@ -1,5 +0,0 @@
-def estimate_tokens(prompt: str) -> tuple[int, float]:
-    num_chars = len(prompt)
-    token_est = num_chars // 4
-    cost_est = round(token_est / 1000 * 0.001, 6)
-    return token_est, cost_est
