88400ee421a48120db73c5ba5af3f0556bcbfa41
MarcoRipari <marco.ripari.falc@gmail.com>
2025-08-08 09:37:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -913,9 +913,6 @@ elif page == "📸 Foto - Gestione":
         st.success("✅ Ristampe confermate per le seguenti SKU:")
         for riga in st.session_state["ristampe_confermate"]:
             st.markdown(f"- {riga}")
-        
-        if st.button("Ricomincia lista ristampe"):
-            del st.session_state["ristampe_confermate"]
     else:
         sku_input = st.text_input("🔍 Inserisci SKU da cercare (solo con foto esistenti)")
         
