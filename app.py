7ff5baac72ea16f4f04f40ee9d183346b5163da6
MarcoRipari <marco.ripari.falc@gmail.com>
2025-08-12 14:32:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -952,7 +952,8 @@ elif page == "📸 Foto - Gestione":
     # ✅ Considera solo SKU che hanno già la foto (SCATTARE == False)
     df_foto_esistenti = df[df["SCATTARE"] == False]
     df_da_riscattare = df[df["SCATTARE"] == True]
-    for row in df_da_riscattare:
+    
+    for _, row in df_da_riscattare.iterrows():
         selected_ristampe.add(row['SKU'])
                      
     if st.session_state.get("ristampe_confermate"):
