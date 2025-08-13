95ec4ac8e30d356fe4cbc54df7f6c6a099e6d171
MarcoRipari <marco.ripari.falc@gmail.com>
2025-08-13 09:53:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -897,7 +897,8 @@ elif page == "Foto - Gestione":
     with col2:
         if st.button("📄 Scarica PDF DISP"):
             # 1️⃣ Filtra il dataframe
-            df_disp = df[df["DISP"] == True]
+            df_disp = df_disp[df["DISP"] == True]
+            df_disp = df_disp["COD","VAR","COL","TG PIC","COR","LAT","X","Y","FOTOGRAFO"]            
             
             if df_disp.empty:
                 st.warning("Nessuna SKU disponibile per DISP.")
