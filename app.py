129c3d6127d53f57d0b39b6db2c65a7088b3486d
MarcoRipari <marco.ripari.falc@gmail.com>
2025-08-14 12:29:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1601,7 +1601,7 @@ elif page == "Giacenze - Per corridoio/marchio":
     df["MARCHIO_STD"] = df["COLLEZIONE"].map(marchi_mapping)
     
     marchi = sorted(df["MARCHIO_STD"].dropna().unique())
-    st.write(marchi)
+    
     # --- Costruisco dati per tabella piatta ---
     table_data = []
     for corr in sorted(df["CORR_NUM"].unique()):
@@ -1622,7 +1622,7 @@ elif page == "Giacenze - Per corridoio/marchio":
             row[f"{brand}_NUOVO"] = nuovo
     
         table_data.append(row)
-    st.write(brand_df)
+        
     df_table = pd.DataFrame(table_data)
     
     # --- Costruzione colonne AgGrid con colori alternati ---
