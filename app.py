e563f292545031f3178bad885404cba088fbd3b4
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-03 18:38:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from urllib.parse import urlparse, parse_qs
 # --------------------------
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_ANON_KEY = st.secrets["SUPABASE_ANON_KEY"]
-REDIRECT_URL = "https://tuo-progetto.streamlit.app/"  # Inserisci l'URL pubblico della tua app Streamlit Cloud
+REDIRECT_URL = "https://gestione-ecom.streamlit.app/"  # Inserisci l'URL pubblico della tua app Streamlit Cloud
 
 supabase = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
