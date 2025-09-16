183f9634c052f44e55db163fd8552c9abccd9653
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-16 17:35:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -984,7 +984,7 @@ def extract_data_from_page(page_text):
     country_match = re.search(r"([A-Z]{2}).*?Spedizioniere", page_text)
 
     st.write(page_text)
-    st.write(country_match)
+    st.write(country_match.group(1).strip())
     
     if not country_match:
         country_match = re.search(r"^([A-Z]{2}) http.*", page_text, re.MULTILINE)
