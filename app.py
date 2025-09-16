ed4310288db33134278062fb8187ebf48c4b77f0
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-16 18:12:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -984,26 +984,7 @@ def extract_data_from_page(page_text):
     # Nazione
     #country_match = re.search(r"\n[0-9]{3}.*([A-Z]{2})\s*$", page_text.strip(), re.MULTILINE)
 
-    address_match = re.search(r"Shipping address\s+Billing address\s+(.*?)(?=Spedizioniere)", page_text, re.DOTALL)
-    
-    st.write(address_match.group(1).strip())
-    st.write(get_country_from_address("Silvia Nick Westumer Strasse 110 SinzigSilvia Nick Westumer Strasse 110 53489 Sinzig DE DEPACKET DE"))
-        
-    address = None
-    if address_match:
-        full_address_block = address_match.group(1)
-        address_lines = [line.strip() for line in full_address_block.split('\n') if line.strip()]
-        
-        # This is a very basic way to get a single address string.
-        address = ' '.join(address_lines[:4])
-    
-    country = get_country_from_address(address)
-    
-    country_match = re.search(r"Billing address\s(.*)Spedizioniere", page_text)
-
-    st.write(address)
-    st.write(country)
-    
+    country_match = re.search(r"([A-Z]{2}).*?Spedizioniere", page_text)
     
     if country_match:
         data['Nazione'] = country_match.group(1).strip()
