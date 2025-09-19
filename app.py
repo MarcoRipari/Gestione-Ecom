3a09d4a12e43a176b3e5b78acbc38c3d5390e67b
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-19 17:21:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2857,7 +2857,7 @@ elif page == "Catalogo - Aggiungi ordini stagione":
             csv = read_csv_auto_encoding(file, separatore=",")
             data = pd.DataFrame(csv)
             data = data[1:]
-
+            st.write(data[0])
             if data["COD.CLIENTI"] == "0019243.016":
                 ecom.append(data)
             elif  data["COD.CLIENTI"] == "0039632":
