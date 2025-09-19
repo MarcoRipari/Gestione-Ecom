173e9d7a4934e229804099130677ecfc390cb0fe
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-19 14:58:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1605,9 +1605,9 @@ elif page == "Foto - Gestione":
     st.session_state["df_lista_foto"] = df
 
     # 1️⃣ Genero le liste per i fotografi
-    df_disp = df[df["DISP"] == True]]
-    df_disp_027 = df[df["DISP 027" == True]]
-    df_disp_028 = df[df["DISP 028" == True]]
+    df_disp = df[df["DISP"] == True]
+    df_disp_027 = df[df["DISP 027"] == True]
+    df_disp_028 = df[df["DISP 028"] == True]
     
     #df_disp = df_disp[["COD","VAR","COL","TG PIC","DESCRIZIONE","COR","LAT","X","Y","FOTOGRAFO"]]
     df_disp = df_disp.sort_values(by=["COR", "X", "Y", "LAT"])
