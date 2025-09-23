cc422093629e69e70050fa69bc20f3e8dc096fa2
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-23 07:22:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1111,7 +1111,7 @@ st.markdown(
     """
     <style>
         section[data-testid="stSidebar"] {
-            width: 500px !important; # Set the width to your desired value
+            width: 300px !important; # Set the width to your desired value
         }
     </style>
     """,
