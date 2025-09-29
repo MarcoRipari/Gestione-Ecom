9c2502ccb2ac61e1f40c74887e4771da7973bdc1
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-29 16:38:26 +0200

    Create supabase.py

---
+++ functions/supabase.py
@@ -0,0 +1,7 @@
+from supabase import create_client, Client
+
+supabase_url = st.secrets["SUPABASE_URL"]
+supabase_key = st.secrets["SUPABASE_KEY"]
+service_role_key = st.secrets["SUPABASE_SERVICE_ROLE_KEY"]
+supabase: Client = create_client(supabase_url, supabase_key)
+supabase_admin = create_client(supabase_url, service_role_key)
