7f9928522c4f9cf62fa55df35e461ba3e8e96f29
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-29 16:47:08 +0200

    Update auth_system.py

--- functions/auth_system.py
+++ functions/auth_system.py
@@ -1,11 +1,7 @@
 import streamlit as st
 from supabase import create_client, Client
 
-supabase_url = st.secrets["SUPABASE_URL"]
-supabase_key = st.secrets["SUPABASE_KEY"]
-service_role_key = st.secrets["SUPABASE_SERVICE_ROLE_KEY"]
-supabase: Client = create_client(supabase_url, supabase_key)
-supabase_admin = create_client(supabase_url, service_role_key)
+
 
 
 def login(username: str, password: str) -> bool:
