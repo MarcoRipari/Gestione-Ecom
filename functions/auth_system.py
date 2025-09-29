806089db19e9edbe5de4ed45616561bd4abe5453
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-29 16:46:08 +0200

    Update auth_system.py

--- functions/auth_system.py
+++ functions/auth_system.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from supabase import create_client, Client
 
+supabase_url = st.secrets["SUPABASE_URL"]
+supabase_key = st.secrets["SUPABASE_KEY"]
+service_role_key = st.secrets["SUPABASE_SERVICE_ROLE_KEY"]
+supabase: Client = create_client(supabase_url, supabase_key)
+supabase_admin = create_client(supabase_url, service_role_key)
+
+
 def login(username: str, password: str) -> bool:
     try:
         # 1. Recupera il profilo dallo username
