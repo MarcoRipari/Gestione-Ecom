663ad12a3f9570d870eb411051268647ca6f426e
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-02 16:57:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2029,7 +2029,7 @@ elif page == "Foto - Aggiungi prelevate":
     st.markdown("Aggiungi la lista delle paia prelevate")
     
     sheet = get_sheet(foto_sheet_id, "PRELEVATE")
-    
+    oggi = datetime.today().date()
     text_input = st.text_area("Lista paia prelevate", height=400, width=800)
     
     if text_input:
@@ -2054,7 +2054,7 @@ elif page == "Foto - Aggiungi prelevate":
     
             if skus_to_append_clean:
                 # Aggiungi apostrofo solo al momento dell'append per forzare formato testo
-                rows_to_append = [[f"'{sku}", "1", "2"] for sku in skus_to_append_clean]
+                rows_to_append = [[f"'{sku}", "=REPO(A33)", oggi, "=SE(VAL.NON.DISP(CONFRONTA(INDIRETTO(\"LISTA!$D\"&CONFRONTA($A2;LISTA!A:A;0));SPLIT(SETTINGS(\"brandMatias\");\",\");0));SE(VAL.NON.DISP(CONFRONTA(INDIRETTO(\"LISTA!$D\"&CONFRONTA($A2;LISTA!A:A;0));SPLIT(SETTINGS(\"brandMatteo\");\",\");0));\"\";\"MATTEO\");\"MATIAS\")"] for sku in skus_to_append_clean]
                 
                 # Append a partire dall'ultima riga disponibile
                 sheet.append_rows(rows_to_append, value_input_option="USER_ENTERED")
