2d664b0fef7427b4e9421a97005a9e399fc60bf2
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-09 08:24:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -258,35 +258,20 @@ def get_blip_caption(image_url: str) -> str:
         return ""
 
 # Carica modello, tokenizer e processor
-model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+processor = BlipProcessor.from_pretrained("noamrot/FuseCap")
+model = BlipForConditionalGeneration.from_pretrained("noamrot/FuseCap").to(device)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-
-max_length = 16
-num_beams = 3
-gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
 def get_blip_caption_new(image_url: str) -> str:
     try:
-        images = []
-        i_image = Image.open(requests.get(image_url, stream=True).raw)
-        if i_image.mode != "RGB":
-            i_image = i_image.convert(mode="RGB")
-        images.append(i_image)
-        
-        pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
-        pixel_values = pixel_values.to(device)
-    
-        output_ids = model.generate(pixel_values, **gen_kwargs)
-    
-        preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-        preds = preds.strip()
+        raw_image = Image.open(requests.get(imgage_url, stream=True).raw).convert('RGB')
+        inputs = processor(raw_image, return_tensors="pt")
+        out = model.generate(**inputs)
+        caption = processor.decode(out[0], skip_special_tokens=True))
     except Exception as e:
-        preds = f"Errore: {e}"
+        caption = f"Errore: {e}"
         
-    return preds
+    return caption
     
 # ---------------------------
 # 🧠 Prompting e Generazione
