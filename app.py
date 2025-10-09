ae8ae35d33ce0437aa8855ca9c97416e2d13ad58
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-09 08:26:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -258,9 +258,8 @@ def get_blip_caption(image_url: str) -> str:
         return ""
 
 # Carica modello, tokenizer e processor
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-processor = BlipProcessor.from_pretrained("noamrot/FuseCap")
-model = BlipForConditionalGeneration.from_pretrained("noamrot/FuseCap").to(device)
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
 def get_blip_caption_new(image_url: str) -> str:
     try:
