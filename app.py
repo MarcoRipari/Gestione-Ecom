197f53fa310930ddcc9501f54698a154579d4773
MarcoRipari <marco.ripari.falc@gmail.com>
2026-01-22 07:49:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3929,7 +3929,7 @@ elif page == "Ferie - Report":
     st.markdown(ferie_report_df_styled.to_html(escape=False), unsafe_allow_html=True)
 
 elif page == "Traduci":
-    st.title("Excel Translator AI 🌍")
+    st.title("Excel Translator AI 🌍2")
     
     uploaded_file = st.file_uploader("Carica .xls", type=["xls"])
 
